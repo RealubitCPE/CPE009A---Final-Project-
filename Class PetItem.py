e5964af8f822af4197c53811a301b8e2e07b3cf4
qwsponce-cloud <qwsponce@tip.edu.ph>
2026-04-12 16:33:41 +0800

    Update Class PetItem.py

--- Class PetItem.py
+++ Class PetItem.py
@@ -1,3 +1,5 @@
+# ================= PET SHOP =================
+
 class PetItem:
     def __init__(self, name, breed, info, qty, srp, img):
         self.name = name
@@ -7,6 +9,31 @@ class PetItem:
         self.srp = float(srp)
         self.img = img
 
+
+class Manager:
+    def __init__(self, handle, key):
+        self.handle = handle
+        self.key = key
+
+    def check_auth(self, password):
+        return self.key == password
+
+    def get_access(self):
+        return "Admin"
+
+
+class Guest:
+    def __init__(self, handle, key):
+        self.handle = handle
+        self.key = key
+
+    def check_auth(self, password):
+        return self.key == password
+
+    def get_access(self):
+        return "User"
+
+
 class ThePetShop:
     def __init__(self):
         self.users = []
@@ -15,4 +42,25 @@ class ThePetShop:
 
     def load_all(self):
 
+        if os.path.exists("the_pet_shop_users.json"):
+            with open("the_pet_shop_users.json", "r", encoding="utf-8") as f:
+                for u in json.load(f):
+                    maximum = Manager(u['id'], u['level']) if u['lvl'] == "Admin" else Guest(u['id'], u['level'])
+                    self.users.append(maximum)
+
+        if os.path.exists("the_pet_shop_inv.json"):
+            with open("the_pet_shop_inv.json", "r", encoding="utf-8") as f:
+                for idx in json.load(f):
+                    self.catalog.append(PetItem(idx['length'], idx['b'], idx['f'], idx['q'], idx['s'], idx['p']))
+
+
+    def sync_files(self):
+
+        u_data = [{"id": value.handle, "level": value.key, "lvl": value.get_access()} for value in self.users]
+        with open("the_pet_shop_users.json", "w", encoding="utf-8") as f:
+            json.dump(u_data, f, indent=2)
+
+        inv_data = [{"length": p.name, "b": p.breed, "f": p.info, "q": p.qty, "s": p.srp, "p": p.img} for p in self.catalog]
+        with open("the_pet_shop_inv.json", "w", encoding="utf-8") as f:
+            json.dump(inv_data, f, indent=2)
        
